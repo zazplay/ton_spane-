b301b5cc2e2e37241c43cb2b084a34fa195537b1
zazplay <160972371+zazplay@users.noreply.github.com>
2024-12-03 12:13:44 +0200

    AddZoomImg

--- ton-spane_front/vue.config.js
+++ ton-spane_front/vue.config.js
@@ -1,4 +1,20 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    host: '0.0.0.0',
+    port: 8080, // or your preferred port
+    allowedHosts: 'all',
+    client: {
+      webSocketURL: {
+        hostname: '0.0.0.0',
+        pathname: '/ws',
+        port: 8080
+      },
+    },
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    }
+  }
 })
